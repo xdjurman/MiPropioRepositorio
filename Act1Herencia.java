1c6724dd15bce2c41136174fc89f1d62eb8e0400
Diego Jurado <xdjurman537@ieshnosmachado.org>
2024-01-21 23:41:13 +0100

    Actualización de archivo

--- Act1Herencia.java
+++ Act1Herencia.java
@@ -1,36 +1,58 @@
 package unidad4;
-
+/**
+ * Act1herencia representa las horas(de 0 a 23) y minutos que tiene el dia, para que devuelva la representación de un reloj
+ * @author Diego Jurado
+ */
 public class Act1Herencia {
 	protected int hora;
 	protected int minutos;
-
+/**
+ * Crea un día recibiendo las horas y los minutos
+ * @param hora hora del dia
+ * @param minutos minutos del dia
+ */
 	public Act1Herencia(int hora, int minutos) {
 		super();
 		setHora(hora);
 		setMinutos(minutos);
 	}
-
+	/**
+	 * Devuelve las horas
+	 * @return return de hora
+	 */
 	public int getHora() {
 		return hora;
 	}
 
-	
+	/**
+	 * Configura el reloj para que muestre de 0 a 23 horas
+	 * @param hora
+	 */
 	public void setHora(int hora) {
 		if (hora >= 0 && hora < 24) {
 			this.hora = hora;
 		}
 	}
-
+	/**
+	 * Devuelve los minutos
+	 * @return return de minutos
+	 */
 	public int getMinutos() {
 		return minutos;
 	}
-
+	
+	/**
+	 * Configura el reloj para que muestre de 0 a 50 minutos
+	 * @param minutos 
+	 */
 	public void setMinutos(int minutos) {
 		if (minutos >= 0 && minutos < 60) {
 			this.minutos = minutos;
 		}
 	}
-	
+	/**
+	 * Configura el reloj para que si sobre pasa los 59 minutos añade una hora al reloj
+	 */
 	public void inc() {
 		if(this.minutos<59) {
 			this.minutos++;
